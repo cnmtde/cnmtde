e43b43ea3bac76345040543277c668453991b731
christian.nusser@mytoys.de <christian.nusser@mytoys.de>
2022-05-07 08:26:33 +0000

    com15

--- models/example/d_customers.sql
+++ models/example/d_customers.sql
@@ -1,6 +1,8 @@
 {{
   config(
-    materialized='table'
+        materialized='incremental',
+        incremental_strategy='merge',
+        merge_update_columns = ['deduplicated_customer_id', 'deduplicated_customer_flag']
   )
 }}
 
