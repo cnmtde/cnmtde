43f06edebde133e65c3c84894c2fd8dd7dddb15d
christian.nusser@mytoys.de <christian.nusser@mytoys.de>
2022-05-07 07:54:49 +0000

    com12

--- models/example/d_customers.sql
+++ models/example/d_customers.sql
@@ -1,6 +1,7 @@
 with d_customers as (
     Select *
     from {{ ref('s_cust_duplicate_check') }} a
+    join {{ ref('l_cust_bill_addresses') }} b on b.lcustbadd_has = a.lcustbadd_has
     join {{ ref('h_customers') }} c on c.hcust_hash = b.hcust_hash
 )
 select * from d_customers
