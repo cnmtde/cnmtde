b5bee9f7607fb2e143ee06734da5862cd57d9a12
christian.nusser@mytoys.de <christian.nusser@mytoys.de>
2022-05-07 08:01:38 +0000

    com14

--- models/example/d_customers.sql
+++ models/example/d_customers.sql
@@ -5,7 +5,9 @@
 }}
 
 with d_customers as (
-    Select distinct *
+    Select distinct c.hcust_hash as customer_id,
+    c.hcust_hash as deduplicated_customer_id,
+    case when a.type_of_duplicate is null then 'Y' end as deduplicated_customer_flag 
     from {{ ref('s_cust_duplicate_check') }} a
     join {{ ref('l_cust_bill_addresses') }} b on b.lcustbadd_hash = a.lcustbadd_hash
     join {{ ref('h_customers') }} c on c.hcust_hash = b.hcust_hash
