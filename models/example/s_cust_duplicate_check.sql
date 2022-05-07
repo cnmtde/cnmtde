ac4c849ed0e73225743cc8eab04e1fd0fdf7146d
christian.nusser@mytoys.de <christian.nusser@mytoys.de>
2022-05-07 07:16:25 +0000

    com10

--- models/example/s_cust_duplicate_check.sql
+++ models/example/s_cust_duplicate_check.sql
@@ -1,8 +1,7 @@
-insert into `gluon-mytoys-sandbox-d.cn_dbt_src.bdv_s_cust_duplicate_check`(lcustbadd_hash,
+insert into `gluon-mytoys-sandbox-d.cn_dbt_src.bdv_s_cust_duplicate_check` (lcustbadd_hash,
 lcustbadd_hash_master,
 duplicate_check_result,
-load_dts
-)
+load_dts)
 select lcustbadd_hash,
 lcustbadd_hash_master,
 duplicate_check_result,
